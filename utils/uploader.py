91cf88e724fbc0ab6edfbd4732761b50cc8b499a
Guodong Liu <gliu@splunk.com>
2018-07-30 14:34:57 +0800

    add face uploader

--- utils/uploader.py
+++ utils/uploader.py
@@ -37,3 +37,34 @@ def upload_face(data):
             file_path = os.path.abspath(file_root_path + file_middle_path + file_sub_path)
 
             im = base_im
+            im = make_thumb(im, size[0])
+
+            if not os.path.exists(file_path):
+                os.makedirs(file_path)
+
+            im.save('%s/%s' %(file_path, file_name), 'PNG')
+
+        _state['success'] = True
+        _state['message'] = file_sub_path + file_name
+    else:
+        _state['success'] = False
+        _state['message'] = '还未选择要上传的文件。'
+
+    return _state
+
+
+def make_thumb(im, size=75):
+    width, height = im.size
+    if width == height:
+        region = im
+    else:
+        if width > height:
+            delta = (width - height)/2
+            box = (delta, 0, delta+height, height)
+        else:
+            delta = (height - width)/2
+            box = (0, delta, width, delta+width)
+        region = im.crop(box)
+
+    thumb = region.resize((size, size), Image.ANTIALIAS)
+    return thumb
